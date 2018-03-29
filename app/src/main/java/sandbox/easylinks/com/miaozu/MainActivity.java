e81a106439b69a62e17a4de30c1c9e9730462953
apple <lwtwan321@126.com>
2018-03-29 14:14:10 +0800

    1

--- app/src/main/java/sandbox/easylinks/com/miaozu/MainActivity.java
+++ app/src/main/java/sandbox/easylinks/com/miaozu/MainActivity.java
@@ -1,6 +1,10 @@
 package sandbox.easylinks.com.miaozu;
 
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import okhttp3.OkHttpClient;
 import sandbox.easylinks.com.miaozu.common.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -15,9 +19,29 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    OkHttpClient client = new OkHttpClient();
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void initData() {
+
+//        Request request = new Request.Builder()
+//                .url("https://raw.github.com/square/okhttp/master/README.md")
+//                .build();
+//        client.newCall(request).enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                String json = response.body().string();
+//                Log.d("okHttp", json);
+//            }
+//        });
+
+
     }
 
 
